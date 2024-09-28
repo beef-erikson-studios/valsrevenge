b04a8f99af9b31944871f2c81bae0b6804ea6f9c
Beef Erikson Studios <troy.martin.jobs@gmail.com>
2024-09-28 15:22:00 -0400

    loadTextures extension method implemented for preparing textures for animations.

--- Extensions/SKTexture+LoadTextures.swift
+++ Extensions/SKTexture+LoadTextures.swift
@@ -5,4 +5,25 @@
 //  Created by Troy Martin on 9/28/24.
 //
 
-import Foundation
+import SpriteKit
+
+extension SKTexture {
+    
+    // Builds an array of textures that will be used for animation.
+    static func loadTextures(atlas: String, prefix: String, startsAt: Int, stopsAt: Int) -> [SKTexture] {
+        
+        // Creates a texture array and atles to store animation textures
+        var textureArray = [SKTexture]()
+        let textureAtlas = SKTextureAtlas(named: atlas)
+        
+        // Loops through start and stop points, applying textures based on prefix and index.
+        for i in startsAt...stopsAt {
+            let textureName = "\(prefix)\(i)"
+            let texture = textureAtlas.textureNamed(textureName)
+            textureArray.append(texture)
+        }
+        
+        // Return the [SKTexture]
+        return textureArray
+    }
+}

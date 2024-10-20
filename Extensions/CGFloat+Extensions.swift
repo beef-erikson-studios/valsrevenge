0d7ceff840744ec5aa100a42a5862340e30570db
Beef Erikson Studios <troy.martin.jobs@gmail.com>
2024-10-20 11:17:15 -0400

    Two clamping versions added - range-based and two-value functions.

--- Extensions/CGFloat+Extensions.swift
+++ Extensions/CGFloat+Extensions.swift
@@ -5,4 +5,21 @@
 //  Created by Troy Martin on 10/20/24.
 //
 
-import Foundation
+import CoreGraphics
+
+extension CGFloat {
+    /// Two-argument version of clamping.
+    func clamped(v1: CGFloat, v2: CGFloat) -> CGFloat {
+        let min = v1 < v2 ? v1 : v2
+        let max = v1 > v2 ? v1 : v2
+        
+        return self < min ? min : (self > max ? max : self)
+    }
+    
+    /// CloseRange version of clamping.
+    func clamped(to range: ClosedRange<CGFloat>) -> CGFloat {
+        let min = range.lowerBound, max = range.upperBound
+        
+        return self < min ? min : (self > max ? max : self)
+    }
+}
